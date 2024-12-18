2d6b93d8b0219dbb12a1e56a8955ab14a9b4ba79
sudheer96s <108125653+sudheer96s@users.noreply.github.com>
2024-12-18 11:02:04 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,3 @@
-# mkdir -p ~/.streamlit/
-
-# echo "\
-# [server]\n\
-# headless = true\n\
-# port = $PORT\n\
-# enableCORS = false\n\
-# " > ~/.streamlit/config.toml
-
-
-
-
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt 
