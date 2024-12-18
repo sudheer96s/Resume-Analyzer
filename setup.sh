4c2057fb1048f1260d88863b3fdbe333470d13be
sudheer96s <108125653+sudheer96s@users.noreply.github.com>
2024-12-18 10:59:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
-mkdir -p ~/.streamlit/
+# mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-" > ~/.streamlit/config.toml
+# echo "\
+# [server]\n\
+# headless = true\n\
+# port = $PORT\n\
+# enableCORS = false\n\
+# " > ~/.streamlit/config.toml
 
+
+
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt 
 python -m spacy download en_core_web_sm
